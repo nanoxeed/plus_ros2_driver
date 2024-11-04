b96b4e585a2a255fd8655002a0211e3aa5026aa7
neka-nat <nekanat.stock@gmail.com>
2024-11-05 00:15:11 +0900

    sensor publisher

--- src/m5base_controller.hpp
+++ src/m5base_controller.hpp
@@ -1,11 +1,15 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joy.hpp>
+#include <std_msgs/msg/float32.hpp>
 
 class M5baseController : public rclcpp::Node
 {
 private:
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_dist_;
+    rclcpp::TimerBase::SharedPtr timer_;
     void joyCallback(const sensor_msgs::msg::Joy& joy);
+    void timerCallback();
 public:
     M5baseController();
 };
--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -13,6 +13,58 @@
 
 #define SERVER_PORT 10200      // 通信ポート
 #define DEF_IP "192.168.0.162" // サーバーIP
+#define UDP_PORT 1234          // UDPポート
+
+
+void *udp_recvfunc(void *pdata)
+{
+    MotorControl *pcontroller = (MotorControl *)pdata;
+    unsigned short port = UDP_PORT;
+    int sockClient = -1;
+    struct sockaddr_in sockAddrInServer;
+
+    while (1)
+    {
+        if (sockClient == -1)
+        {
+            memset(&sockAddrInServer, 0, sizeof(sockAddrInServer));
+            sockAddrInServer.sin_port = htons(port);
+            sockAddrInServer.sin_family = AF_INET;
+            sockAddrInServer.sin_addr.s_addr = INADDR_ANY;
+
+            sockClient = socket(AF_INET, SOCK_DGRAM, 0);
+            if (sockClient < 0)
+            {
+                printf("ERROR:socket\n");
+                usleep(100000);
+                continue;
+            }
+            if (bind(sockClient, (struct sockaddr *)&sockAddrInServer, sizeof(sockAddrInServer)) != 0)
+            {
+                printf("ERROR:bind\n");
+                close(sockClient);
+                sockClient = -1;
+                usleep(100000);
+                continue;
+            }
+
+            pcontroller->m_bUdpConnected = true;
+        }
+        else
+        {
+            char buff[256];
+            int len = recv(sockClient, buff, sizeof(buff), 0);
+            if (len > 0)
+            {
+                pcontroller->m_sensor_mutex.lock();
+                pcontroller->m_dist = atof(buff);
+                printf("UDP:dist=%f\n", pcontroller->m_dist);
+                pcontroller->m_sensor_mutex.unlock();
+            }
+        }
+    }
+}
+
 
 void *threadfunc(void *pdata)
 {
@@ -53,6 +105,7 @@ void *threadfunc(void *pdata)
         else
         {
             char buff[256];
+            pcontroller->m_command_mutex.lock();
             sprintf(buff, "###,%3d,%3d,%03d,%03d\r", pcontroller->m_r, pcontroller->m_l, 0, 0);
             if (send(sockClient, buff, ((int)strlen(buff) + 0), 0) < 0)
             {
@@ -60,6 +113,7 @@ void *threadfunc(void *pdata)
                 break;
             }
             printf("SC:send[%s]\n", buff);
+            pcontroller->m_command_mutex.unlock();
 
             memset(buff, 0, sizeof(buff));
             int len = recv(sockClient, buff, 7, 0);
@@ -89,6 +143,19 @@ void MotorControl::start()
 void MotorControl::drive(int rval, int lval)
 {
     printf("JOY STICK MESSAGE %d %d\n", rval, lval);
+    set_commands(rval, lval);
+}
+
+float MotorControl::get_dist()
+{
+    m_sensor_mutex.lock();
+    return m_dist;
+}
+
+void MotorControl::set_commands(int rval, int lval)
+{
+    m_command_mutex.lock();
     m_r = rval;
     m_l = lval;
+    m_command_mutex.unlock();
 }
